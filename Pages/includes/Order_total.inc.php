150d9a56b155340a7afcbedaa70158ffd1cd8eb5
Houssam Righi <hrighi01@gmail.com>
2022-12-09 03:40:12 -0500

    submit + display orders

--- Pages/includes/Order_total.inc.php
+++ Pages/includes/Order_total.inc.php
@@ -1,7 +1,80 @@
 <?php
 
+    include_once 'dbConfig.inc.php';
+    session_start();
     // Makes sure the user accessed this file through the submit button
     if (isset($_POST["order_submit"])) {
-        $prices = $_POST["add_quantity"];
-        echo $prices;
+        $company_name = $_POST["company_name"];
+        $product_name = $_POST["product_name"];
+        $product_quantity = $_POST["order_quantity"];
+        $product_quantity_available = $_POST["product_quantity_available"];
+        $product_price = $_POST["product_price"];
+
+        $order_total = 0;
+        for ($i=0; $i < count($product_name); $i++) {
+            $order_total += ($product_quantity[$i] * $product_price[$i]);
+        }
+
+        // for ($i=0; $i < count($product_name); $i++) { 
+        //     echo $product_name[$i];
+        //     echo "<br>";
+        //     echo $product_quantity[$i];
+        //     echo "<br>";
+        //     echo $product_quantity_available[$i];
+        //     echo "<br>";
+        //     echo $product_price[$i];
+        //     echo "<br>";
+        // }
+        // ($_SESSION["client_id"], date("j-m-y"), $order_total)
+
+        $sql1 = "INSERT INTO orders (client_id, order_date, order_total) VALUES (?, ?, ?);";
+        $stmt = mysqli_stmt_init($conn);
+
+        if (!mysqli_stmt_prepare($stmt, $sql1)) {
+            header("location: ../index.php?error=stmt1failed");
+            exit();
+        }
+        
+        $clientID = $_SESSION["client_id"];
+        $today = date("j-m-y");
+
+        mysqli_stmt_bind_param($stmt, "sss",  $clientID, $today, $order_total);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_close($stmt);
+
+        $last_id = mysqli_insert_id($conn);
+    
+        // header("location: ../index.php?error=none");
+        // exit();
+
+        $sql2 = "SELECT supplier_id FROM suppliers WHERE company_name='$company_name[0]';";
+        $result2 = mysqli_query($conn, $sql2);
+        
+        $supplier_id = "";
+        $product_ids = [];
+        
+        while($row = mysqli_fetch_assoc($result2)) {
+            $supplier_id = $row["supplier_id"];
+        }
+
+        for ($i=0; $i < count($product_name); $i++) { 
+            $sql3 = "SELECT product_id FROM products WHERE supplier_id=$supplier_id AND product_name='$product_name[$i]';";
+            $result3 = mysqli_query($conn, $sql3);
+
+            while($row = mysqli_fetch_assoc($result3)) {
+                $product_ids[$i] = $row["product_id"];
+            }
+
+            $sql4 = "INSERT INTO order_list (order_id, product_id, product_quantity) VALUES (?, ?, ?);";
+            $stmt = mysqli_stmt_init($conn);
+
+            if (!mysqli_stmt_prepare($stmt, $sql4)) {
+                header("location: ../index.php?error=stmt2failed");
+                exit();
+            }
+        
+            mysqli_stmt_bind_param($stmt, "sss", $last_id, $product_ids[$i],  $product_quantity[$i]);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_close($stmt);
+        }
     }
